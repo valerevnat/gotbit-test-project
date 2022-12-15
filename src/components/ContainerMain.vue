d5a448866a3c8da070cb66a0352e8a1b1cbe4139
Tanya <valerevnatat@gmail.com>
2022-12-15 19:30:11 +0300

    conect metaMAsk, signer, address, provider. Add ether.js

--- src/components/ContainerMain.vue
+++ src/components/ContainerMain.vue
@@ -1,39 +1,96 @@
 <script setup lang="ts">
 
 import { ref } from 'vue';
+import { ethers } from 'ethers';
+
 import ModalWindow from './base/ModalWindow.vue';
 import BasicContent from './BasicContent.vue';
 import ButtonComponent from './base/ButtonComponent.vue';
-import ConectMetaMask from './ConectMetaMask.vue'
-// import ToolTip from './base/ToolTip.vue'
+import ConectMetaMask from './ConnectMetaMask.vue'
+
+// const CONTRACT_ADDRESS = '0x12431F18Cb02163342AB8FBed5d43c30Da5e59bC';
+// const ABI = [
+//     "function balanceOf(address owner) view returns (uint256)",
+// ];
 
 
 const show = ref<boolean>(false)
 const content = ref<string>('basic-content')
 
+
 const handlerShowModal = () => {
     show.value = !show.value
 }
 
-const handlerConect = () => {
-    content.value = 'conect-metaMask'
-    console.log('show', show.value);
-    console.log('conect', content.value);
+
+// MetaMask перенести в пиниа 
+
+// const instanceOwner = ref('');
+// const offerPrice = ref('');
 
 
+// сохранить текущий адрес кошелька
+const walletAddress = ref<string>('');
+
+async function requestAccount() {
+    console.log('Request account...');
+    // проверяем, если метамаск существует
+    if (window.ethereum) {
+        console.log('MetaMask is installed!')
+        try {
+            const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+            walletAddress.value = accounts[0]
+            console.log(accounts);
+
+        } catch (error) {
+            console.log('Error connecting...', error);
+        }
+    } else {
+        console.log('MetaMask not detected');
+
+    }
 
 }
 
-// const tooltip = ref<boolean>(false)
-// const showTooltip = () => {
-//     tooltip.value = !tooltip.value
-// }
+async function connectWallet() {
+    // проверяем, существует ли матемаск
+    if (typeof window.ethereum !== 'undefined') {
+        await requestAccount();
 
+        //С помощью провайдера мы подключаемся к сети Blockcain
+        const provider = new ethers.providers.Web3Provider(window.ethereum); // этот провайдер мы можем использвать для взаимодействия со смарт-кантрактом
+
+        //Дает нам возможность читать view методы контракта
+        // const readOnlyContract = new ethers.Contract()
+        // console.log('readOnlyContract', readOnlyContract);
+
+
+        //signer нужен для подтверждения транзакций
+        const signer = provider.getSigner();
+        // const erc20 = new ethers.Contract(CONTRACT_ADDRESS, ABI, provider);
+        // const erc20_rw = new ethers.Contract(CONTRACT_ADDRESS, ABI, signer);
+
+    }
+}
 
 </script>
 
 <template>
     <div class="container">
+        <div>Wallet address: {{ walletAddress }}</div>
+
+        <!-- удалить блок -->
+        <!-- <div id="app"> -->
+        <!-- <button v-on:click="details">Get details</button> -->
+        <!-- <h3>Instance owner: {{ instanceOwner }}</h3> -->
+        <!-- <h3>Description: {{ description }}</h3>
+            <h3>Asking price: {{ askingPrice }}</h3>
+            <h3>Instance buyer: {{ instanceBuyer }}</h3>-->
+        <!-- <h3>Offer price: {{ offerPrice }}</h3> -->
+        <!-- </div> -->
+        <!-- удалить блок -->
+
+
         <div class="title">
             <div class="title-left">Flexible</div>
             <div class="title-right">Staking</div>
@@ -42,10 +99,16 @@ const handlerConect = () => {
             <BasicContent v-if="content === 'basic-content'" :handlerShowModal="handlerShowModal" />
             <ConectMetaMask v-if="content === 'conect-metaMask'" />
             <ModalWindow @showModal="handlerShowModal" :show="show">
-                <ButtonComponent variant='btn-metamask' @click="handlerConect">
+                <ButtonComponent variant='btn-metamask' @click="requestAccount">
                     <div class="modal-content-main">
                         <img src="../assets/img/logo-mask.png" alt="MetaMask" />
                         <div class="modal-content-main-text">MetaMask</div>
+                    </div>
+                </ButtonComponent>
+                <ButtonComponent variant='btn-metamask' @click="connectWallet">
+                    <div class="modal-content-main">
+                        <img src="../assets/img/logo-mask.png" alt="MetaMask" />
+                        <div class="modal-content-main-text">MetaMask Provider</div>
                     </div>
                 </ButtonComponent>
             </ModalWindow>
